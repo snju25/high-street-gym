b580e0eb7346d81dd34f6b4e7059ff6b70cf8ac1
snju25 <bhandarisanjay879@gmail.com>
2024-02-17 20:53:33 +1000

    finished navbar

--- frontend/src/pages/HomeLayout.jsx
+++ frontend/src/pages/HomeLayout.jsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
 
 const HomeLayout = () => {
   return (<>
-    <div>HomeLayout</div>
-    <Outlet />
+    <Header/>
+    <Navbar/>
+    <div className="align-element py-20">
+      <Outlet />
+    </div>
   </>
   )
 }
