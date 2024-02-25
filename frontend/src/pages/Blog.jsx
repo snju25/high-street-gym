1ba5a5d10e217406b91e6a90983dacb49b1a9ce6
snju25 <bhandarisanjay879@gmail.com>
2024-02-26 08:44:54 +1000

    created blog page

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,6 +1,55 @@
+import {Form, Link} from "react-router-dom"
+import InputForm from "../components/InputForm"
+const testData = [
+  {
+    blogId:1,
+    username: "Sanjay",
+    title: "First blog post..................................",
+  },
+  {
+
+    blogId: 2,
+    username: "Sanjay",
+    title: "First blog post..................................",
+  },{
+    blogId: 3,
+    username: "Sanjay",
+    title: "First blog post..................................",
+  },
+  {
+    blogId: 4,
+    username: "Sanjay",
+    title: "First blog post..................................",
+  }
+]
+
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <main>
+      <section>
+        {/* Post a blog */}
+        <div className="card rounded-lg shadow-lg border border-red-100 h-[100px] mb-5 ">
+          <Form>
+            <InputForm label="title" placeholder="create a blog here..." />
+
+          </Form>
+
+        </div>
+      </section>
+      <section className="flex flex-col gap-5">
+          {testData.map((blog,i)=>{
+            return <section key={i} className="flex gap-3">
+              <img src="https://images.pexels.com/photos/15294904/pexels-photo-15294904/free-photo-of-portrait-of-brown-cat.jpeg" alt="default user photo" className="h-[40px] w-[40px] rounded-full object-cover"/>
+              <div className="flex justify-center flex-col">
+                <h1>{blog.username}</h1>
+                <p>{blog.title}</p>
+              </div>
+              <Link to={`/blog/${blog.blogId}`} className="btn btn-primary ml-auto p-0 px-2 py-1">Read More</Link>
+            </section>
+          })}
+        
+      </section>
+    </main>
   )
 }
 export default Blog
--- frontend/src/pages/SingleBlogPage.jsx
+++ frontend/src/pages/SingleBlogPage.jsx
@@ -1,6 +1,30 @@
+
+// export const loader = async ({params}) =>{
+//   const {id} = params
+//   const response = await customFetch(`/products/${id}`)
+//   const product = response.data.data
+//   return {product}
+
+import { Link } from "react-router-dom"
+
+// }
+const sampleData = {
+    blogId:1,
+    username: "Sanjay",
+    title: "First blog post..................................",
+    subtitle:"I am 30  i want to make a career change. What should i do",
+    content:"The blog post Content here................."
+  }
+
 const SingleBlogPage = () => {
   return (
-    <div>SingleBlogPage</div>
+    <div>
+      <section className="flex items-center gap-3"><Link to='/blog'> <span className="font-bold text-3xl">&#8592;</span> </Link> <span className="text-4xl font-bold">{sampleData.title}</span></section>
+      <article className="mt-5">
+        <h1 className="text-3xl font-bold">{sampleData.subtitle}</h1>
+        <p className="">{sampleData.content}</p>
+      </article>
+    </div>
   )
 }
 export default SingleBlogPage
