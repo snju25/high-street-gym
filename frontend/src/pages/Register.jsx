44a0abab7e9070278211da6852fe9f04575a4c82
snju25 <bhandarisanjay879@gmail.com>
2024-04-20 18:06:27 +1000

    validated

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -30,11 +30,11 @@ const Register = () => {
       <Form method="post" className="card w-96 md:w-[80%] max-w-[900px] p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"> 
       <h4 className="text-center text-3xl font-bold">Register</h4>
       <div className="grid md:grid-cols-2 md:gap-2">
-        <InputForm type="email" label="email" name="email" errorMessage="Invalid input..." />
-        <InputForm type="password" label="password" name="password" errorMessage="invalid input...." />
-        <InputForm type="text" label="phone" name="phone" errorMessage="Invalid input...."/>
-        <InputForm type="text" label="first Name" name="firstName" errorMessage="Invalid input...."/>
-        <InputForm type="text" label="Last Name" name="lastName" errorMessage="Invalid input...."/>
+        <InputForm type="email" label="email" name="email" pattern="^\S+@\S+\.\S+$" errorMessage="Invalid input must be a valid email address" />
+        <InputForm type="password" label="password" name="password" pattern="[a-zA-Z0-9\-]{6,}" errorMessage="invalid password must be 6 character long" />
+        <InputForm type="text" label="phone" name="phone" pattern="(^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$)" errorMessage="Invalid phone number"/>
+        <InputForm type="text" label="first Name" name="firstName" pattern="[a-zA-Z-]{2,}" errorMessage="Invalid first Name must be all letters"/>
+        <InputForm type="text" label="Last Name" name="lastName" pattern="[a-zA-Z-]{2,}" errorMessage="Invalid last Name must be all letters"/>
         <InputForm type="text" label="Address" name="address" errorMessage="Invalid input...."/>
       </div>
       <div className="mt-4">
