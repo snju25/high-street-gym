8ff65fe21ad4756c6d5833d03f6022532d4df2c9
snju25 <bhandarisanjay879@gmail.com>
2024-04-26 20:27:45 +1000

    deploy link changes

--- frontend/src/utils/axios/axios.js
+++ frontend/src/utils/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
  const customFetch = axios.create({
-    baseURL: "http://localhost:8081/",
+    baseURL: "https://high-street-gym-54sr.onrender.com/",
     headers: {
         Accept: 'application/json',
       },
