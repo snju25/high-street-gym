e6a4799f2933410e0c9645cf026fc064e951dbb7
snju25 <bhandarisanjay879@gmail.com>
2024-04-27 16:12:29 +1000

    last connection

--- backend/src/database.js
+++ backend/src/database.js
@@ -4,6 +4,6 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const URL_DATABASE = `mysql://root:jcsZMNxPBZdWcosAJhpVRBeRFmvpxqLG@viaduct.proxy.rlwy.net:26192/railway`
-export const db = mysql.createConnection(URL_DATABASE)
+export const db = mysql.createPool(URL_DATABASE)
 
 // Example of error handling
